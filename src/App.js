82eba5393f45e6aaa2a3eba0da4fac70b8498244
Owais Naqvi <65233517+OwaisHaider896@users.noreply.github.com>
2020-10-06 21:33:23 +0500

    add icon and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
+import MovieCreationIcon from '@material-ui/icons/MovieCreation';
 
 export default function App() {
   const [search, setSearch] = useState("");
@@ -12,9 +13,9 @@ export default function App() {
       const response = await fetch(
         `https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}`
       );
-      const { results } = await response.json();
-      setMovie(results);
-      console.log(results);
+      const data = await response.json();
+      setMovie(data.results);
+      //console.log(data);
     } catch (error) {
       console.log(error);
     }
@@ -22,6 +23,10 @@ export default function App() {
 
   return (
     <div className="App">
+      <div className="main">
+        < MovieCreationIcon />
+      <h2>MovieFinder</h2>
+      </div>
       <input
         type="text"
         className="search"
@@ -36,14 +41,21 @@ export default function App() {
         Search
       </button>
 
-      {movie.map((value) => (
-        <ul>
-          <li>{value.title}</li>
-
-          <p>{value.overview} </p>
-          <img src={value.backdrop_path} alt={value.title} />
-        </ul>
-      ))}
+      <ul className="list">
+        <div className="info">
+          {movie.map((value) => (
+            <li>
+              <h4>{value.title} </h4>
+              <img
+                className="image"
+                src={`https://image.tmdb.org/t/p/w500${value.poster_path}`}
+                alt={value.title}
+              />
+              
+            </li>
+          ))}
+        </div>
+      </ul>
     </div>
   );
 }
@@ -53,3 +65,5 @@ export default function App() {
 //https://cors-anywhere.herokuapp.com/
 
 //c58c4c307b856ef0a1879f3e9b3ef58a
+
+//https://image.tmdb.org/t/p/w500$
